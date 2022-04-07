59167b0cfe4cda9144a69d3f574674fa2c9e919c
Victor Pena <victorpenawow@gmail.com>
2022-04-07 14:53:06 +0200

    Añadido params y foreach

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,5 +1,5 @@
 package com.bolsadeideas.springboot.web.app.controllers;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -51,12 +52,23 @@ public class IndexController {
 	
 	@RequestMapping("/listar")
 	public String listar(Model model) {
-		List<Usuario> usuarios = new ArrayList<>();		
+		List<Usuario> usuarios = Arrays.asList(new Usuario("Andres", "Guzman", "andres@correo.com"), new Usuario("Jhon", "Doe", "jhon@correo.com"), new Usuario("Jane", "Pena", "jane@correo.com"));
+		/*List<Usuario> usuarios = new ArrayList<>();		
+		usuarios.add(new Usuario("Andres", "Guzman", "andres@correo.com"));
+		usuarios.add(new Usuario("Jhon", "Doe", "jhon@correo.com"));
+		usuarios.add(new Usuario("Jane", "Pena", "jane@correo.com"));*/
+		
 		
 		model.addAttribute("titulo", "Listado Usuarios");
 		model.addAttribute("usuarios", usuarios);
 		
-		return "listar";
+		return "listar";		
+	}
+	
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios(){
+		List<Usuario> usuarios = Arrays.asList(new Usuario("Andres", "Guzman", "andres@correo.com"), new Usuario("Jhon", "Doe", "jhon@correo.com"), new Usuario("Jane", "Pena", "jane@correo.com"));
 		
+		return usuarios;
 	}
 }
